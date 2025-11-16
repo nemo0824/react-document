ed5421748894193b323e191b1312308521cba554
임재원 <nemo0824@naver.com>
2025-11-16 22:53:58 +0900

    study: react useState 다중, 커링, 및 클로저

--- 2-hook/src/App.tsx
+++ 2-hook/src/App.tsx
@@ -3,13 +3,18 @@ import MyReact from "./lib/MyReact";
 // const App = () => <>2-hook</>;
 
 function NameField() {
-  const [firstName, setFirstName] = MyReact.useName("사용자1");
+  const [firstName, setFirstName] = MyReact.useState("사용자1");
+  const [lastName, setLastName] = MyReact.useState("김");
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFirstName(e.target.value);
   };
 
-  return <input value={firstName} onChange={handleChange} />;
+  return;
+  <>
+    <input value={firstName} onChange={handleChange} />
+    <input value={lastName} onChange={handleChange}></input>
+  </>;
 }
 
 export default NameField;
--- 2-hook/src/lib/MyReact.tsx
+++ 2-hook/src/lib/MyReact.tsx
@@ -1,31 +1,39 @@
 import React from "react";
 
 const MyReact = (function MyReact() {
-  let firstName: any;
-  let initalized = false;
-  function useName<T>(initialValue: T): [T, (newValue: T) => void] {
+  const memorizeStates: any[] = [];
+  const isInitialized: any[] = [];
+  let cursor = 0;
+  function useState<T>(initialVlaue = "") {
     const { forceUpdate } = useForceUpdate();
-    if (!initalized) {
-      firstName = initialValue;
-      initalized = true;
+    if (!isInitialized[cursor]) {
+      memorizeStates[cursor] = initialVlaue;
+      isInitialized[cursor] = true;
     }
 
-    const setFirstName = (newValue: T) => {
-      if (firstName === newValue) return;
-      firstName = newValue;
+    const state = memorizeStates[cursor];
+
+    const setStateAt = (_cursor: number) => (nextState: any) => {
+      if (state === nextState) return;
+      memorizeStates[_cursor] = nextState;
       forceUpdate();
     };
+    const setState = setStateAt(cursor);
 
-    return [firstName as T, setFirstName];
+    cursor += 1;
+    return [state, setState];
   }
 
   function useForceUpdate() {
     const [value, setValue] = React.useState(1);
-    const forceUpdate = () => setValue(value + 1);
+    const forceUpdate = () => {
+      setValue(value + 1);
+      cursor = 0;
+    };
     return { forceUpdate };
   }
 
-  return { useName };
+  return { useState };
 })();
 
 export default MyReact;
